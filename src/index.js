0c6834d94756e99588f4ee0b11232ef96048389c
da255 <asdfmovieciip@gmail.com>
2024-12-24 15:11:27 -0500

    log in complete

---
+++ src/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+const {Client, Events, GatewayIntentBits, } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds, 
+    ]
+ });
+
+client.on(Events.ClientReady, () => {
+    console.log('logged in');
+});
+
+client.login();
+
